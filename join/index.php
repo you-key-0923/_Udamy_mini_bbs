bca361c6cbc38479a38e08ba1846d76de54204f7
you-key0923 <arborARBOR18@gmail.com>
2019-05-23 11:51:02 +0900

    最初！

---
+++ join/index.php
@@ -0,0 +1,80 @@
+<?php
+
+//$_POSTが空じゃなかった時に、下記のerrorチェックを走らせる
+	//***--「!」付けると逆になる。$_POSTが空じゃなかったら、になる。
+if(!empty($_POST)){
+	//???--なんで$errorは配列なの？
+	if($_POST['name'] === ''){
+		$error['name'] = 'blank';
+	}
+	if($_POST['email'] === ''){
+		$error['email'] = 'blank';
+	}
+	if(strlen($_POST['password']) < 4){
+		$error['password'] = 'length';
+	}
+	if($_POST['password'] === ''){
+		$error['password'] = 'blank';
+	}
+	//errorがなかったら、「check.php」にジャンプする
+	//empty→()内が空か確認する
+	if(empty($error)){
+	header('Location: check.php');
+	exit();
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>会員登録</title>
+
+	<link rel="stylesheet" href="../style.css" />
+</head>
+<body>
+<div id="wrap">
+<div id="head">
+<h1>会員登録</h1>
+</div>
+
+<div id="content">
+<p>次のフォームに必要事項をご記入ください。</p>
+<form action="" method="post" enctype="multipart/form-data">
+	<dl>
+		<dt>ニックネーム<span class="required">必須</span></dt>
+		<dd>
+			<input type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['name'],ENT_QUOTES)); ?>" />
+			<?php if($error['name'] === 'blank'): ?>
+			<p class="error">※ニックネームを入力してください</p>
+			<?php endif; ?>
+		</dd>
+		<dt>メールアドレス<span class="required">必須</span></dt>
+		<dd>
+			<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>" />
+			<?php if($error['email'] === 'blank'): ?>
+			<p class="error">※メールアドレスを入力してください</p>
+			<?php endif; ?>
+		<dt>パスワード<span class="required">必須</span></dt>
+		<dd>
+			<input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'],ENT_QUOTES)); ?>" />
+			<?php if($error['password'] === 'blank'): ?>
+			<p class="error">※パスワードを入力してください</p>
+			<?php endif; ?>
+			<?php if($error['password'] === 'length'): ?>
+			<p class="error">※パスワードは4文字以上で入力してください</p>
+			<?php endif; ?>
+        </dd>
+		<dt>写真など</dt>
+		<dd>
+        	<input type="file" name="image" size="35" value="test"  />
+        </dd>
+	</dl>
+	<div><input type="submit" value="入力内容を確認する" /></div>
+</form>
+</div>
+</body>
+</html>
